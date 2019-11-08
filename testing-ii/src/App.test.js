8fb10c0d18578c5abd50cbe8eb646a6a5608fddb
Daniel Cruz <d.cruz.489@hotmail.com>
2019-11-07 21:30:33 -0700

    basic tests

--- testing-ii/src/App.test.js
+++ testing-ii/src/App.test.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import App from './App';
+import Dashboard from './components/Dashboard'
+import Display from './components/Display'
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 
 
+test('renders without crashing', () => {
+  const wrapper = rtl.render(<App/>)
+  expect(wrapper).toBeVisible;
+});
 
+test('renders dashboard component', () => {
+  const wrapper = rtl.render(<Dashboard/>)
+  const element = wrapper.getByText(/hit/i)
+  expect(element).toBeInTheDocument();
+}) 
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
-
+test('renders display component', () => {
+  const wrapper = rtl.render(<Display/>)
+  const element = wrapper.getByText(/Balls/i)
+  expect(element).toBeInTheDocument();
+}) 
